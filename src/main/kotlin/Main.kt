80157d84ffbf1c7b171500464a652b8500f2041c
Toni773 <75640559+AntonYakushenko@users.noreply.github.com>
2022-08-02 17:41:41 +0400

    Change steps

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,40 @@
-import kotlin.reflect.KFunction
 import kotlin.reflect.full.declaredFunctions
 
-
-/*Создайте класс реализующий интерфейс
-Внутри класса переопределите метод runTest
-Внутри этого метода необходимо сначала вызвать методы с before* из steps, далее прогнать тест (запустить передаваемую функцию test), и после вызвать методы с after* из steps.
-Вызовы before* и after* нужно обернуть в лог/печать в консоль.
-Проверьте работоспобоность написанного кода (можно в методе main).*/
-
 fun main() {
-   //val KClass<*>.declaredFunctions: Collection<KFunction<*>>
-    val steps = Steps::class.declaredFunctions.filter { it.name.startsWith("before")}
-    println(steps)
     MyTestRunner().runTest(Steps(), ::testFun)
-
 }
 interface TestRunner <T> {
-    fun runTest(steps: Any, test: () -> Unit) }
+    fun runTest(Steps: Any,  test: () -> Unit) }
 
 class MyTestRunner : TestRunner <Any> {
 
-    override fun runTest(steps: Any, test: () -> Unit) {
-        Steps().beforeTest()
+    override fun runTest(obj: Any, test: () -> Unit) {
+        steps(obj, "before")
         test()
-        Steps().afterTest()
+        steps(obj, "after")
+    }
+    private fun steps (obj: Any ,prefix : String){
+        val steps = Steps::class.declaredFunctions.filter { it.name.startsWith(prefix)}
+        steps.forEach { it.call(obj) }
     }
 }
 
 class Steps {
 
-   fun beforeTest() {
-    println("before test")
+    fun beforeTest() {
+        println("before test")
     }
 
     fun afterTest() {
-       println("after test")
-
+        println("after test")
     }
 }
+
 fun testFun() {
-    println("testing")
-}
+        println("testing")
+    }
+
+
 
 
 
